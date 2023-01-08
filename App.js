d06a48c8edfa9c481754bab19e4f918c636f441a
Elozino <ovedhee@gmail.com>
2023-01-08 05:15:53 +0100

    update

--- App.js
+++ App.js
@@ -1,9 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// @refresh reset
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import {
+  Button,
+  LogBox,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { db } from "./config/firebaseConfig";
+// import { collection, getDocs } from "firebase/firestore";
 
 export default function App() {
+  const [user, setUser] = useState(null);
+  const [name, setName] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  // const chatsRef = collection(db, "chats");
+
+  async function readUser() {
+    const user = await AsyncStorage.getItem("user");
+    if (user) {
+      setUser(JSON.parse(user));
+    }
+  }
+
+  async function handlePress() {
+    const _id = Math.random().toString(36).substring(7);
+    const user = { _id, name };
+    await AsyncStorage.setItem(
+      "user",
+      JSON.stringify("user", JSON.stringify(user))
+    );
+    setUser(user);
+  }
+
+  useEffect(() => {
+    readUser();
+    // const unsubscribe = getDocs(chatsRef).then((snapshot) => {
+    //   let collection = [];
+    //   snapshot.docs.forEach((doc) => {
+    //     collection.push({
+    //       ...doc.data(),
+    //       // createdAt: doc.data().createdAt.toDate(),
+    //       id: doc.id,
+    //     });
+    //   });
+      // collection.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
+      // setMessages(collection);
+    // });
+  }, []);
+
+  if (!user) {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your name"
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
+        <Button title="Enter the chat" onPress={handlePress} />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
+      <Text>Comfort</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +79,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 30,
+  },
+  input: {
+    height: 50,
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "gray",
+    padding: 15,
+    marginBottom: 20,
   },
 });
---
+++ config/firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMmJb8s480zWdT1tFra_uJzNaG0Su46s4",
+  authDomain: "fp-news-c3cbf.firebaseapp.com",
+  projectId: "fp-news-c3cbf",
+  storageBucket: "fp-news-c3cbf.appspot.com",
+  messagingSenderId: "751970636669",
+  appId: "1:751970636669:web:facd87188ec787aaf9b214",
+  measurementId: "G-GRGK93G6F6",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig); 
+export const db = getFirestore(app);
